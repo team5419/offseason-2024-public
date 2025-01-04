eaebde4b9a759621259492f162f2b4723cb2f239
Rebecca <119640842+rift10@users.noreply.github.com>
2025-01-04 00:10:38 -0800

    build update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2024-offseason-public";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = -1;
-    public static final String GIT_SHA = "UNKNOWN";
-    public static final String GIT_DATE = "UNKNOWN";
-    public static final String GIT_BRANCH = "UNKNOWN";
-    public static final String BUILD_DATE = "2025-01-04 00:04:16 PST";
-    public static final long BUILD_UNIX_TIME = 1735977856772L;
-    public static final int DIRTY = 129;
+    public static final int GIT_REVISION = 1;
+    public static final String GIT_SHA = "62140ea10e535388b7f3f39e0f6cb113b9926873";
+    public static final String GIT_DATE = "2025-01-04 00:06:46 PST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2025-01-04 00:09:10 PST";
+    public static final long BUILD_UNIX_TIME = 1735978150827L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }
